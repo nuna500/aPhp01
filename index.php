7f48ff629fba0621a5040283d74e079a35acd3ee
nuna500 <36657291+nuna500@users.noreply.github.com>
2020-11-02 15:37:42 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,40 +3,79 @@
 <head>
  <meta charset="UTF-8">
 </head>
- <?php
-   $LINEData = file_get_contents('php://input');
-  $jsonData = json_decode($LINEData,true);
+ <?php 
+	/*Get Data From POST Http Request*/
+	$datas = file_get_contents('php://input');
+	/*Decode Json From LINE Data Body*/
+	$deCode = json_decode($datas,true);
+ 
+	$replyToken = $deCode['events'][0]['replyToken'];
+	$userId = $deCode['events'][0]['source']['userId'];
+	$text = $deCode['events'][0]['message']['text'];
 
-  $replyToken = $jsonData["events"][0]["replyToken"];
-  $userID = $jsonData["events"][0]["source"]["userId"];
-  $replyText="GG";
-  function sendMessage($replyJson, $sendInfo){
-          $ch = curl_init($sendInfo["URL"]);
-          curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-          curl_setopt($ch, CURLINFO_HEADER_OUT, true);
-          curl_setopt($ch, CURLOPT_POST, true);
-          curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-              'Content-Type: application/json',
-              'Authorization: Bearer ' . $sendInfo["AccessToken"])
-              );
-          curl_setopt($ch, CURLOPT_POSTFIELDS, $replyJson);
-          $result = curl_exec($ch);
-          curl_close($ch);
-    return $result;
-  }
+	$messages = [];
+	$messages['replyToken'] = $replyToken;
+	$messages['messages'][0] = getFormatTextMessage("เอ้ย ถามอะไรก็ตอบได้");
 
-  
-  $lineData['URL'] = "https://api.line.me/v2/bot/message/reply";
-  $lineData['AccessToken'] = "Ek0Jo6EYTVLSTeQlCiRe29lu7WZ4BJeG6NprVBCMz4RdB6K2rdCAoc5myPf551x7i6L6OPaTVz744tsqv4h/TNO2xfPJXxMd9fI4J3taNgqFLmwU5IhqBiszasnkt5xEzOUV1ZE4agbtisNhxAofTgdB04t89/1O/w1cDnyilFU=";
-  $replyJson = [];
-  $replyJson["replyToken"] = $replyToken;
-  $replyJson["messages"][0] = $replyText;
+	$encodeJson = json_encode($messages);
 
-  $encodeJson = json_encode($replyJson);
+	$LINEDatas['url'] = "https://api.line.me/v2/bot/message/reply";
+  	$LINEDatas['token'] = "Ek0Jo6EYTVLSTeQlCiRe29lu7WZ4BJeG6NprVBCMz4RdB6K2rdCAoc5myPf551x7i6L6OPaTVz744tsqv4h/TNO2xfPJXxMd9fI4J3taNgqFLmwU5IhqBiszasnkt5xEzOUV1ZE4agbtisNhxAofTgdB04t89/1O/w1cDnyilFU=";
 
-  $results = sendMessage($encodeJson,$lineData);
-  echo $results;
-  http_response_code(200); 
+  	$results = sentMessage($encodeJson,$LINEDatas);
+
+	/*Return HTTP Request 200*/
+	http_response_code(200);
+
+	function getFormatTextMessage($text)
+	{
+		$datas = [];
+		$datas['type'] = 'text';
+		$datas['text'] = $text;
+
+		return $datas;
+	}
+
+	function sentMessage($encodeJson,$datas)
+	{
+		$datasReturn = [];
+		$curl = curl_init();
+		curl_setopt_array($curl, array(
+		  CURLOPT_URL => $datas['url'],
+		  CURLOPT_RETURNTRANSFER => true,
+		  CURLOPT_ENCODING => "",
+		  CURLOPT_MAXREDIRS => 10,
+		  CURLOPT_TIMEOUT => 30,
+		  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		  CURLOPT_CUSTOMREQUEST => "POST",
+		  CURLOPT_POSTFIELDS => $encodeJson,
+		  CURLOPT_HTTPHEADER => array(
+		    "authorization: Bearer ".$datas['token'],
+		    "cache-control: no-cache",
+		    "content-type: application/json; charset=UTF-8",
+		  ),
+		));
+
+		$response = curl_exec($curl);
+		$err = curl_error($curl);
+
+		curl_close($curl);
+
+		if ($err) {
+		    $datasReturn['result'] = 'E';
+		    $datasReturn['message'] = $err;
+		} else {
+		    if($response == "{}"){
+			$datasReturn['result'] = 'S';
+			$datasReturn['message'] = 'Success';
+		    }else{
+			$datasReturn['result'] = 'E';
+			$datasReturn['message'] = $response;
+		    }
+		}
+
+		return $datasReturn;
+	}
 ?>
 <body>
  Hello heroku!
